8f6316947dfb6b35fed6ffdac0c989bba4f1094d
AikaOnimura <aika.onimura@gmail.com>
2020-09-16 04:48:43 +0000

    [Start/Add]2-3まで

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,22 @@
+class TodolistsController < ApplicationController
+  
+  def new
+    @list = List.new # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成。
+  end
+  
+  def create
+    #①データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    #②データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    #③トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'todolists/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top' #URL「top」にアクセスした時にhomesコントローラーのtopアクションが呼び出される、という設定。
+  post 'todolists' => 'todolists#create' #「.../todolists」というURLにpostメソッドでリクエストしたときに、todolistsコントローラーのcreateアクションが呼び出される。
+end
