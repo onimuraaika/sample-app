bd42dafe1e5486ba61714d1e25b0f8d33b1732c0
AikaOnimura <aika.onimura@gmail.com>
2020-09-17 03:06:57 +0000

    [Add/Finish]2-3 モデルとデータベースの作成

--- db/migrate/20200915070112_create_lists.rb
+++ db/migrate/20200915070112_create_lists.rb
@@ -1,8 +1,10 @@
 class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
-      t.string :title
-      t.string :body
+# ⬆️ ここまでは元々書いてある
+      t.string :title #カラム(縦1列)の名前を「title」
+      t.string :body #カラムの名前を「body」
+# ⬇️ ここまでは元々書いてある
       t.timestamps
     end
   end
